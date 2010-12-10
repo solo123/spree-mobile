24978321aa78f6299046448ff5ed178b3b20d805
Jimmy@cp.ubu <jimmy@coolpur.com>
2010-12-10 11:33:15 +0800

    images

--- app/controllers/admin/mobiles_controller.rb
+++ app/controllers/admin/mobiles_controller.rb
@@ -7,4 +7,10 @@ class Admin::MobilesController < Admin::BaseController
     txt = MobileHelper.merge(params[:ids]) ? "已成功合并！" : "合并失败！"
     render :text => params[:ids].join(',').to_s + txt
   end
+  def image_order
+    img = Asset.find(params[:image_id])
+    img.position = params[:order]
+    img.save
+    render :text => img.position
+  end
 end
